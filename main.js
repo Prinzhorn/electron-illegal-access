4ffd0cefe05af3a01a7a7a07f14b62616b33cb81
Alexander Prinzhorn <alexander@prinzhorn.it>
2020-09-28 18:59:13 +0200

    more minimal example

--- main.js
+++ main.js
@@ -2,27 +2,9 @@ const path = require('path');
 const { Worker } = require('worker_threads');
 const { app, BrowserWindow } = require('electron');
 
-let worker = null;
+let worker = new Worker(path.join(__dirname, 'server.js'));
 let mainWindow = null;
 
 app.on('ready', () => {
-  worker = new Worker(path.join(__dirname, 'server.js'));
   mainWindow = new BrowserWindow();
-});
-
-let cleanedUp = false;
-
-app.on('before-quit', async function (e) {
-  if (!cleanedUp) {
-    e.preventDefault();
-    console.log('cleaning up...');
-
-    worker.once('message', () => {
-      console.log('cleaned up, quiting');
-      cleanedUp = true;
-      app.quit();
-    });
-
-    worker.postMessage('close');
-  }
 });
--- server.js
+++ server.js
@@ -1,15 +1,3 @@
-const path = require('path');
-const { parentPort } = require('worker_threads');
-
-let timer = setInterval(() => {
-  console.log('server is bae');
+setInterval(() => {
+  console.log('hello from server');
 }, 1000);
-
-parentPort.on('message', async (message) => {
-  console.log('closing server...');
-  clearInterval(timer);
-  console.log('closed server');
-  parentPort.postMessage({
-    type: 'exit',
-  });
-});
